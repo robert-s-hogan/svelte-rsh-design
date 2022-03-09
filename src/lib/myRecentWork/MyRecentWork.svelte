11dfd3096d4e8151be7c56ce5792364168277ed2
Robert Hogan <rhogan7@gmail.com>
2022-03-09 09:46:07 -0800

    Fixed coming soon link

--- src/lib/myRecentWork/MyRecentWork.svelte
+++ src/lib/myRecentWork/MyRecentWork.svelte
@@ -76,12 +76,13 @@
               <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Construction Site</h1>
               <p class="leading-relaxed mb-3">Sample construction site for local businesses to show off their work!</p>
               <div class="flex items-center flex-wrap ">
-                <a target="_blank" rel="nofollower noopener"  disabled href="/#coming-soon" class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Coming Soon
+                <!-- <a disabled href="#" class="text-indigo-500 inline-flex items-center md:mb-2 disabled lg:mb-0"> -->
+                  <p class="italic text-xs">Coming Soon</p>
                   <!-- <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path d="M5 12h14"></path>
                     <path d="M12 5l7 7-7 7"></path>
                   </svg> -->
-                </a>
+                <!-- </a> -->
                 <!-- <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
                   <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
                     <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
