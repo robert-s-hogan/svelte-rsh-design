dfdebcc5f4c6a5bf76b6ecdff73a725d4cdd55db
Robert Hogan <rhogan7@gmail.com>
2022-03-22 20:55:46 -0700

    Changed border color and width.

--- src/lib/myRecentWork/MyRecentWork.svelte
+++ src/lib/myRecentWork/MyRecentWork.svelte
@@ -8,7 +8,7 @@
       </p>
       <div class="flex flex-wrap -m-4">
         <div class="p-4 md:w-1/3">
-          <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <div class="h-full border border-black border-opacity-40 rounded-lg overflow-hidden">
             <img class="lg:h-64 md:h-36 w-full object-contain object-center" loading="lazy" src="https://rshogan.imgix.net/projects/tailwindcss-cofee-shop/rsh_coffee_hero.png?auto=compress,format&w=400&h=256" height={256} width={400} alt="Local Coffee Shop Template">
             <div class="p-6">
               <h3 class="title-font text-lg font-medium text-gray-900 mb-3">Local Coffee Shop</h3>
@@ -37,7 +37,7 @@
           </div>
         </div>
         <div class="p-4 md:w-1/3">
-          <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <div class="h-full border border-black border-opacity-40 rounded-lg overflow-hidden">
             <img class="lg:h-64 md:h-36 w-full object-cover object-center" loading="lazy" src="https://rshogan.imgix.net/projects/svelte-tailwindcss-product-template/product_design.png?auto=compress,format&w=400&h=256" height={256} width={400} alt="Product Shop Template">
             <div class="p-6">
               <h3 class="title-font text-lg font-medium text-gray-900 mb-3">Product Shop</h3>
@@ -65,7 +65,7 @@
           </div>
         </div>
         <div class="p-4 md:w-1/3">
-          <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <div class="h-full border border-black border-opacity-40 rounded-lg overflow-hidden">
             <img class="lg:h-64 md:h-36 w-full object-cover object-center" loading="lazy" src="https://rshogan.imgix.net/projects/svelte-tailwindcss-deck-project/deck_design.png?auto=compress,format&w=400&h=256" height={256} width={400} alt="Construction Site Template">
             <div class="p-6">
               <h3 class="title-font text-lg font-medium text-gray-900 mb-3">Construction Site</h3>
